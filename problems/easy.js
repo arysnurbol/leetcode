ca4305b5e1b50bb44fff3ab010e0758309b8086c
arysnurbol <amit.nurbol@gmail.com>
2021-11-20 23:41:24 +0600

    singleNonDuplicate and two sum

--- problems/easy.js
+++ problems/easy.js
@@ -1,1 +1,34 @@
 console.log('easy')
+
+//1. Two Sum
+
+//Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// let nums = [2,7,11,15], target = 9;
+
+//Example 2:
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+
+// Example 3:
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// var twoSum = function(nums, target) {
+//     let indxs = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             let a = target - nums[i];
+            
+//             if (nums[j] === a) {
+//                 indxs.push(i,j);
+//             }
+//         }
+//     }
+    
+//     return indxs;
+// };
+
+// console.log(twoSum(nums, target));
--- problems/medium.js
+++ problems/medium.js
@@ -1,1 +1,29 @@
-console.log('medium')
+console.log('medium');
+
+// 540. Single Element in a Sorted Array
+
+//Example 1:
+// Input: nums = [1,1,2,3,3,4,4,8,8]
+// Output: 2
+// let nums = [1,1,2,3,3,4,4,8,8];
+
+// Example 2:
+// Input: nums = [3,3,7,7,10,11,11]
+// Output: 10
+
+
+// var singleNonDuplicate = function(nums) {
+//     let obj = {};
+    
+//     for (let i = 0; i < nums.length; i++) {
+//     	if (obj[nums[i]] === nums[i]) {
+//       	delete obj[nums[i]];
+//       } else {
+//       	obj[nums[i]] = nums[i];
+//       }
+//     }
+    
+//    return Object.values(obj)[0];
+// };
+
+// console.log(singleNonDuplicate(nums));
