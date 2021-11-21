e025612d95302eea78d39c5578e32c986f4fef2c
arysnurbol <amit.nurbol@gmail.com>
2021-11-21 10:49:45 +0600

    weekly contest 268

--- problems/easy.js
+++ problems/easy.js
@@ -32,3 +32,44 @@ console.log('easy')
 // };
 
 // console.log(twoSum(nums, target));
+
+/***************************************************************** */
+
+//5201. Watering Plants
+//Example 1:
+// Input: plants = [2,2,3,3], capacity = 5
+// Output: 14
+
+//Example 2
+// Input: plants = [1,1,1,4,2,3], capacity = 4
+// Output: 30
+
+//Example 3:
+// Input: plants = [7,7,7,7,7,7,7], capacity = 8
+// Output: 49
+
+// var wateringPlants = function(plants, capacity) {
+//     let len = plants.length;
+//   let t = capacity;
+  
+//   let step = 0;
+//   let flag = false;
+//     for (let i = 0; i < len; i++) {
+//     if (capacity < plants[i]) {
+//     	flag = true;
+//       capacity = t;
+//     }
+//   	if (flag) {
+//     	step = step + (i + 1) + i;
+//       flag = false;
+//     } else {
+//     	step++;
+//     }
+//     capacity = capacity - plants[i];
+    
+//   }
+  
+// 	return step;
+// };
+
+// console.log(wateringPlants(plants, capacity));
